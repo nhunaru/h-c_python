89da6c89a235fc9ae9534533bd3acb158120478c
macbook <leengocthai2306@gmail.com>
2025-02-18 05:51:37 +0900

    xep loai

--- toan_tu_dk.py
+++ toan_tu_dk.py
@@ -1,3 +1,12 @@
 x=int(input("nhap so nguyen x bat ki:"))
 kq="so chan" if x%2==0 else "so le"
 print("x la ",kq)
+if x>=9:
+    print("hsg")
+elif x>=7:
+    print("hsk")
+elif x>5:
+    print ("hstb")
+else:
+    print("yeu")
+print ("ket thuc chuong trinh")
