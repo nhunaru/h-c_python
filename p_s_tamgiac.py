cb530ffa13879ecd58bb84ee62d8c8b73f88a81d
nhunaru <leengocthai2306@gmail.com>
2025-02-18 23:15:40 +0900

    Update p_s_tamgiac.py

--- p_s_tamgiac.py
+++ p_s_tamgiac.py
@@ -9,7 +9,7 @@ a=math.sqrt((x2-x1)**2+(y2-y1)**2)
 b=math.sqrt((x3-x1)**2+(y3-y1)**2)
 c=math.sqrt((x3-x2)**2+(y3-y2)**2)
 if not (a+b>c and a+c>b and b+c>a) :
-    print("3 ddieem da cho ko tao thanh tam giac")
+    print("3 diem da cho ko tao thanh tam giac")
 else:
     p=(a+b+c)/2
     s=math.sqrt(p*(p-a)*(p-b)*(p-c))
