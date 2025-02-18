b6016323f3cc6849599bcc5a891057f9c658413c
PC GAMEG <ryugakuse23072000@se23072000gmail.com>
2025-02-18 23:20:06 +0900

    nice

--- p_s_tamgiac.py
+++ p_s_tamgiac.py
@@ -1,10 +1,10 @@
 import math
-x1=float(input("nhap x1:"))
-y1=float(input("nhap y1:"))
-x2=float(input("nhap x2:"))
-y2=float(input("nhap y2:"))
-x3=float(input("nhap x3:"))
-y3=float(input("nhap y3:"))
+x1=float(input("nhap gtri x1:"))
+y1=float(input("nhap gtri y1:"))
+x2=float(input("nhap gtri x2:"))
+y2=float(input("nhap gtri y2:"))
+x3=float(input("nhap gtri x3:"))
+y3=float(input("nhap gtri y3:"))
 a=math.sqrt((x2-x1)**2+(y2-y1)**2)
 b=math.sqrt((x3-x1)**2+(y3-y1)**2)
 c=math.sqrt((x3-x2)**2+(y3-y2)**2)
@@ -15,4 +15,3 @@ else:
     s=math.sqrt(p*(p-a)*(p-b)*(p-c))
     print(f"chu vi cuar tam giac la:{a+b+c:.2f}")
     print(f"dien tich tam giac la:{s:.2f}")
-            
