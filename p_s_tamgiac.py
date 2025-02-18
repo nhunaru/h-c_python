b66f2814a70a335ec1d078ac91c4f8bafad4a148
nhunaru <ryugakuse23072000@se23072000gmail.com>
2025-02-18 23:13:35 +0900

    mtamgiac

---
+++ p_s_tamgiac.py
@@ -0,0 +1,18 @@
+import math
+x1=float(input("nhap x1:"))
+y1=float(input("nhap y1:"))
+x2=float(input("nhap x2:"))
+y2=float(input("nhap y2:"))
+x3=float(input("nhap x3:"))
+y3=float(input("nhap y3:"))
+a=math.sqrt((x2-x1)**2+(y2-y1)**2)
+b=math.sqrt((x3-x1)**2+(y3-y1)**2)
+c=math.sqrt((x3-x2)**2+(y3-y2)**2)
+if not (a+b>c and a+c>b and b+c>a) :
+    print("3 ddieem da cho ko tao thanh tam giac")
+else:
+    p=(a+b+c)/2
+    s=math.sqrt(p*(p-a)*(p-b)*(p-c))
+    print(f"chu vi cuar tam giac la:{a+b+c:.2f}")
+    print(f"dien tich tam giac la:{s:.2f}")
+            
