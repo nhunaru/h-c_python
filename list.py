215c81098a5ceffee7907802b261f79baeb6d9f1
PC GAMEG <ryugakuse23072000@se23072000gmail.com>
2025-02-19 06:14:02 +0900

    thêm file

---
+++ list.py
@@ -0,0 +1,13 @@
+colors=["red","blue","yellow"]
+print(colors)
+student=["an","binh","nga","phuong"]
+print(student[0])
+print(student[2])
+print(student[:])
+print(student[1:2])
+#chèn vào cuối chuỗi
+student.append("ngọc")
+#chèn vào vị trí chỉ định
+student.insert(2,"nghĩa")
+print(student)
+print(len(student))
